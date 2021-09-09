4d90433a0ec320685c42d30c62313c0ba3fe6803
Soo Philip Kim <philipjkim@gmail.com>
2021-09-09 17:48:46 +0900

    Fix broken test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Ant Design/i);
+  const linkElement = getByText(/Player List/i);
   expect(linkElement).toBeInTheDocument();
 });
